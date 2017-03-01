f4af541c472bf5d3b3e39048294c75924f1298f3
bunkermaster <yann.le-scouarnec@hetic.net>
2017-03-01 15:52:31 +0100

    A faire pour la semaine prochaine

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php
+include "includes/header.php";
+?>
+        <div class="jumbotron">
+            <h1><?php // ici le H1?></h1>
+            <p><?php // ici la description?></p>
+        </div>
+        <img class="img-thumbnail" alt="<?php // ici le img-alt?>" src="img/<?php // ici le img-src?>" data-holder-rendered="true">
+<?php
+include "includes/footer.php";
+?>
