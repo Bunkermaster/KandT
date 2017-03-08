d5ee4a6e6ab4574a62ea2c7eccc02384d3ecd68d
bunkermaster <yann.le-scouarnec@hetic.net>
2017-03-08 15:10:48 +0100

    solution partielle

--- template.php
+++ template.php
@@ -3,9 +3,9 @@ include "includes/header.php";
 ?>
         <div class="jumbotron">
             <h1><?=$row['h1']?></h1>
-            <p><?php // ici la description?></p>
+            <p><?=$row['description']?></p>
         </div>
-        <img class="img-thumbnail" alt="<?php // ici le img-alt?>" src="img/<?php // ici le img-src?>" data-holder-rendered="true">
+        <img class="img-thumbnail" alt="<?=$row['img-alt']?>" src="img/<?=$row['img-src']?>" data-holder-rendered="true">
 <?php
 include "includes/footer.php";
 ?>
