8cf0893d89570b560a59107c0730a20366775267
bunkermaster <yann.le-scouarnec@hetic.net>
2017-03-15 14:38:01 +0100

    Debut d'admin

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php
+// @todo lister les pages avec les champs suivants visibles dans une TABLE HTML: slug, h1
+
+// on se connecte à la base de données
+// select des pages (avec slug et h1)
+// preparation
+// execution
+// fetch en boucle pour generer la liste
