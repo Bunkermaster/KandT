21b555f3a37e66c5893001b1b75277f663e4eda2
bunkermaster <yann.le-scouarnec@hetic.net>
2017-03-15 16:13:25 +0100

    last commit HAHAHHHAHHA

--- admin/add.php
+++ admin/add.php
@@ -1,2 +1,17 @@
 <?php
-var_dump($_POST);
+require_once "../connect.php";
+$sql = "INSERT INTO 
+`page`
+(`slug`, `nav-title`, `h1`, `description`, `img-alt`, `img-src`)
+VALUES 
+(:slug, :navtitle, :h1, :description, :imgalt, :imgsrc)
+";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':slug', $_POST['slug']);
+$stmt->bindValue(':navtitle', $_POST['nav-title']);
+$stmt->bindValue(':h1', $_POST['h1']);
+$stmt->bindValue(':description', $_POST['description']);
+$stmt->bindValue(':imgalt', $_POST['img-alt']);
+$stmt->bindValue(':imgsrc', $_POST['img-src']);
+$stmt->execute();
+header("Location: index.php");
